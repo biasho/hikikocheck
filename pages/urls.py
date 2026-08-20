7cc02c9e42713cae3582ab109cc7d1662a258b57
biasho <mr.thanhbinhnguyen@gmail.com>
2026-08-20 08:29:29 +0700

    Fea: cap nhat trang about_us va guide

--- pages/urls.py
+++ pages/urls.py
@@ -5,6 +5,7 @@ app_name = 'pages'
 
 urlpatterns = [
     path('', views.home, name='home'),         # URL: /
+    path('guide/', views.guide, name='guide'), # URL: /about/
     path('about/', views.about, name='about'), # URL: /about/
     path('about-us/', views.about_us, name='about_us'),   # Trang Về chúng tôi
 ]
